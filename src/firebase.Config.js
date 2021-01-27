7cea0e2274b0fbd251a01ab5749b4a1bf2658af3
Israa Ahmad <isra.ah.im@hotmail.com>
2021-01-27 23:10:20 +0200

    last commit

---
+++ src/firebase.Config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCSGJC3xWxusZyYwJqIg-ULCcsadZy4458",
+  authDomain: "dineln-44cc5.firebaseapp.com",
+  projectId: "dineln-44cc5",
+  storageBucket: "dineln-44cc5.appspot.com",
+  messagingSenderId: "253116402635",
+  appId: "1:253116402635:web:0c1baa4c497b6a5eb1444e",
+  measurementId: "G-BK9PTJZBTZ"
+};
+  export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+import 'firebase/auth';
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
